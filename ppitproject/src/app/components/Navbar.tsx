f4b04c4536b6393be3f40b71bc13ec5dce3d4313
David Nolan <davidnolan458@gmail.com>
2024-07-27 15:53:46 +0100

    add basic Navbar

---
+++ ppitproject/src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+    return <div id='navbar'>
+        <a href='https://example.com'>Home</a>
+        <a href='https:/Games.com'>Games</a>
+        <a href='/login'>Log-in</a>
+        <a href='https:/Games.com'>Cart</a>
+        <a href='https://example.com'>Sign Up</a>
+    </div> 
+    
+    
+    
+
+}
---
+++ ppitproject/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar"
+
+export default function index() {
+ return <div>
+     <Navbar />
+    <h1>Hello, Home page!</h1>
+  
+  
+  </div>
+ 
+  
+}
