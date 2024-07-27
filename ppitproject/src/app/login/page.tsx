d75eae65301ee303dcfee1d8b14726ae1532dc6f
David Nolan <davidnolan458@gmail.com>
2024-07-27 15:54:22 +0100

    add basic form

---
+++ ppitproject/src/app/login/page.tsx
@@ -0,0 +1,17 @@
+export default function Login() { 
+    
+    return <div id='login'>
+        <form>
+           <label>username</label>
+            <input type="text" placeholder="Username"/>
+            <label>Password</label>
+            <input type="password" placeholder="Password"/>
+            <button>Log in</button>
+        </form>
+   
+    </div> 
+
+
+    
+
+}
