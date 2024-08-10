fc77dea4c6773e8432d5cabef9b25cfcb120328c
David Nolan <davidnolan458@gmail.com>
2024-08-10 04:04:23 +0100

    latest changes

--- ppitproject/src/app/games/page.tsx
+++ ppitproject/src/app/games/page.tsx
@@ -1,6 +1,18 @@
+'use client'
+import axios from 'axios';
+import React, { use, useEffect, useState } from 'react';
+
 export default function Games() {
-   
-   
+   const [games, setGames] = useState<any>([]);
+   useEffect(() => {
+
+    (async () => {
+        const response = await axios.get('http://localhost:3000/games.json')
+        setGames(response.data);
+
+
+    })();
+   }, []);
    
    return  <div>
   
@@ -9,11 +21,18 @@ export default function Games() {
     <h1>Games</h1>
     </div>
      <div id='games'>
-        
+        {games.map((game) => {
+
+            return <div className='thumbnail'>
+             <span className='game-title'>{game.title}</span>
+             <span className='game-description'>{game.short_description}</span>
+             </div>
+        })}
+
+    
+        {/* <div className='thumbnail'></div>
         <div className='thumbnail'></div>
-        <div className='thumbnail'></div>
-        <div className='thumbnail'></div>
-        <div className='thumbnail'></div>
+        <div className='thumbnail'></div> */}
        
    
     </div>
