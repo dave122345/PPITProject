938be1e0e125105356cd107e6c56ef32cb5985ab
David Nolan <davidnolan458@gmail.com>
2024-08-16 15:46:17 +0100

    frontend: display currently logged in username

--- ppitproject/src/app/components/Navbar.tsx
+++ ppitproject/src/app/components/Navbar.tsx
@@ -1,10 +1,28 @@
+"use client"
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export function Navbar() {
+    const [user, setUser] = useState<any>(null)
+    useEffect(() => {
+        async function fetchUser() {
+            const {data} = await axios.get('/api/user')
+            setUser(data)
+        }
+        fetchUser()
+    }, [])
+
     return <div id='navbar'>
         <a href='/'>Home</a>
         <a href='/games'>Games</a>
         <a href='/login'>Log-in</a>
         <a href='https:/Games.com'>Cart</a>
         <a href='/signup'>Sign Up</a>
+        
+        {user && <div>
+            <span>Logged in as: </span>
+            <span>{user.username}</span>
+        </div>}
     </div> 
     
     
