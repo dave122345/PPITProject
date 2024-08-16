a80a80a0c9fb01aeb2ec5d434903c26a349adc68
David Nolan <davidnolan458@gmail.com>
2024-08-16 16:17:58 +0100

    display logout button

--- ppitproject/src/app/components/Navbar.tsx
+++ ppitproject/src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { LogoutButton } from "./LogoutButton"
 
 export function Navbar() {
     const [user, setUser] = useState<any>(null)
@@ -15,9 +16,10 @@ export function Navbar() {
     return <div id='navbar'>
         <a href='/'>Home</a>
         <a href='/games'>Games</a>
-        <a href='/login'>Log-in</a>
         <a href='https:/Games.com'>Cart</a>
-        <a href='/signup'>Sign Up</a>
+      { !user &&   <a href='/login'>Log-in</a>}
+      { user &&  <LogoutButton />}
+      { !user &&  <a href='/signup'>Sign Up</a>}
         
         {user && <div>
             <span>Logged in as: </span>
