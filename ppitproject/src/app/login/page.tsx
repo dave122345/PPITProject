e67aaddf4f727819a229210578db7ee0d44fd0cd
David Nolan <davidnolan458@gmail.com>
2024-08-16 15:47:17 +0100

    frontend: user log in form

--- ppitproject/src/app/login/page.tsx
+++ ppitproject/src/app/login/page.tsx
@@ -1,15 +1,40 @@
+"use client"
 
+import axios from "axios"
+import { useRouter } from 'next/navigation'
+import { useState } from "react"
 
 export default function Login() { 
-    
+    const router = useRouter()
+
+    const [response, setResponse] = useState<any>(null)
+   
+   
+    async function onSubmit(event) {
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget)
+        const values = Object.fromEntries(formData)
+        console.log('form data sending to backend', values)
+        const {data} = await axios.post('/api/login', values)
+        setResponse(data)
+        console.log(data)
+        if (data.success) {
+            
+            window.location.href = '/'
+        }
+        
+
+
+    }
     return <div id='login'>
         
-        <form>
+        <form onSubmit={onSubmit}>
            <label>username</label>
-            <input type="text" placeholder="Username"/>
+            <input type="text" name="username" placeholder="Username"/>
             <label>Password</label>
-            <input type="password" placeholder="Password"/>
-            <button>Log in</button>
+            <input type="password" name="password" placeholder="Password"/>
+            <input type="submit" value="Log in"/>
         </form>
    
     </div> 
